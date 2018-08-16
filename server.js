25e01adacff49a478c6f1286706fca1ebd016893
Jeff Lucas <jslucas1@msn.com>
2018-08-15 19:17:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/', (req, res) => {
 require('./app/routes/player.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log("Server is listening on port 3000");
 });
